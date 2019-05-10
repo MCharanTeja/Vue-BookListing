cfdffb4a8ade143df7a32085765cb312ca3cc035
Charanteja.m <Charanteja.m@tadigital.com>
2019-05-10 16:16:44 +0530

    Completed the module

---
+++ src/components/BookItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+            <li>{{book.title}}:{{book.author}}</li>   
+    </div>
+</template>
+<script>
+    export default {
+        name: 'BookItem',
+        prop: ['book']
+    }
+</script>
+<style>
+    li {
+        display: block;
+        margin: 0 10px;
+    }
+</style>
